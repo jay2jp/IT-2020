45d9027cab206d0925e72ba8c8ceaac0653710dc
jay2jp <jay2jp@gmail.com>
2020-02-22 23:31:07 -0500

    small ammount of work done

--- Project1/rs.py
+++ Project1/rs.py
@@ -5,6 +5,27 @@ import socket
 
 class rs:
     def rsListenPort(self):
-        dnsTable = {'hostName': '', 'IPadress': socket.gethostbyname(socket.gethostname()), 'flag': ''}
+        try:
+            ss = socket.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+            print("[S]: Server socket created")
+        except socket.error as err:
+            print('{} \n'.format("socket open error ", err))
+        server_binding = ('', 50007)
+        ss.bind(server_binding)
+        ss.listen(1)
+        host = socket.gethostname()
+        print("[S]: Server host name is: ", host)
+        localhost_ip = (socket.gethostbyname(host))
+        csockid, addr = ss.accept()
+        print("[S]: Server IP address is  ", localhost_ip)
+        dnsTable = {
+            'qtsdatacenter.aws.com': ['128.64.3.2','A'],
+            'mx.rutgers.edu': ['192.64.4.2', 'A'],
+            'kill.cs.rutgers.edu': ['182.48.3.2', 'A'],
+            'www.ibm.com': ['64.42.3.4','A'],
+            'google.com': ['8.6.4.2', 'A'],
+            'localhost': ['', 'NS']
+            }
+
 
 
